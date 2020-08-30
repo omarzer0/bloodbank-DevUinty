997332311d3d2f99c4a7e76abc9ab6cee948d01d
Omar Adel <omarzero1015@gmail.com>
2020-08-30 14:27:59 +0200

    first

---
+++ app/src/main/java/com/azapps/bloodbank_devunity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.azapps.bloodbank_devunity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
